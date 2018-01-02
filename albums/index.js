de7f707b3fe5922a86ebe07194ef83cd713b9640
Jeff Guerrera <jdguerrera@gmail.com>
2018-01-01 19:17:02 -0500

    Initial Commit

---
+++ albums/index.js
@@ -0,0 +1,17 @@
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry, View } from 'react-native'; // just access to Text from react-native library
+// need to specify full path for user-defined libaries/modules
+import Header from './src/components/Header'; 
+import AlbumList from './src/components/AlbumList';
+
+// Create a component (object/function that can be rendered to screen)
+const App = () => (
+	<View>
+		<Header headerText={'Albums'} />
+		<AlbumList />
+	</View>
+);
+
+// Render component to the screen (must register at least one component)
+AppRegistry.registerComponent('albums', () => App);
---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+	// class level property 
+	state = { albums: [] }; // initial or empty state with no albums
+
+	// lifecycle methods
+	componentWillMount() {
+		//console.log('componentWillMount');
+		// this will add breakpoint
+		// debugger;
+
+		// set the state to include albums api data
+		// props are for parent/child data xfer
+		// state is for intra-component data 
+		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+			.then(response => this.setState({ albums: response.data }));
+	}
+
+	renderAlbums() { 
+		// key would normally be a unique identifier
+		return this.state.albums.map(album => 
+			<AlbumDetail key={album.title} album={album} />
+		); 
+	}
+
+	render() {
+		console.log(this.state);
+
+		return (
+			<View>
+				{this.renderAlbums()}
+			</View>	
+		);
+	}
+}
+
+export default AlbumList;
