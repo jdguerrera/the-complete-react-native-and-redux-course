d0a4abdac39d98d2d851fc54a643d80b774628f7
Jeff Guerrera <jdguerrera@gmail.com>
2018-01-12 13:36:20 -0500

    Lecture 103

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { 
+	Text, 
+	TouchableWithoutFeedback, 
+	View,
+	LayoutAnimation
+} from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
+	componentWillUpdate() {
+		LayoutAnimation.spring();
+	}
+
+	renderDescription() {
+		const { library, expanded } = this.props;
+
+		if (expanded) {
+			return (
+				<CardSection>
+					<Text style={{ flex: 1 }}>
+						{library.description}
+					</Text>
+				</CardSection>
+			);
+		}
+	}
+
 	render() {
 		const { titleStyle } = styles
+		const { id, title } = this.props.library;
+
 
 		return (
-			<CardSection>
-				<Text style={titleStyle}>
-					{this.props.library.title}
-				</Text>
-			</CardSection>
+			<TouchableWithoutFeedback 
+				onPress={() => this.props.selectLibrary(id)}
+			>
+				<View>
+					<CardSection>
+						<Text style={titleStyle}>
+							{title}
+						</Text>
+					</CardSection>
+					{this.renderDescription()}
+				</View>
+			</TouchableWithoutFeedback>
 		);
 	}
 }
@@ -23,4 +57,14 @@ const styles = {
 	}
 }
 
-export default ListItem;
+// to consume state from redux, map state to props
+// own props is same as this.props inside mapStateToProps
+const mapStateToProps = (state, ownProps) => {
+	//is the library id the same as the selected id from the reducer?
+	const expanded = state.selectedLibraryId == ownProps.library.id;
+	return { expanded };
+};
+
+// first argument is for mapStateToProps, pass null
+// second argument is action creators and passes to component as props
+export default connect(mapStateToProps, actions)(ListItem);
--- tech_stack/src/reducers/SelectionReducer.js
+++ tech_stack/src/reducers/SelectionReducer.js
@@ -1,4 +1,14 @@
-export default () => {
+// set state default to null for first run --REQUIRED--
+export default (state = null, action) => {
 	// set initial state when app boots; cannot be undefined
+	console.log(action);
+	switch (action.type) {
+		case 'select_library':
+			return action.payload;
+
+		// catch all in case an action comes in that shouldn't
+		default:
+			return state;
+	}
 	return null;
 };
